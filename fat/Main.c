56c79075cdbf71af202a05f9e7041d4dfdc69f8f
최 원현 <coy4728@naver.com>
2019-08-12 17:52:34 +0900

    file 보기, 찾기 등 추가

--- fat/Main.c
+++ fat/Main.c
@@ -11,7 +11,7 @@ void User_Main(void);
 void Main(void)
 {
 	MMU_Init();
-	Uart_Init(115200);	
+	Uart_Init(115200);
 	printf("\n=================================\n");
 	main();
 	printf("=================================\n");
@@ -27,19 +27,19 @@ void Main(void)
 
 /* 512B 단위의 섹터를 인쇄해주는 디버깅용 함수 */
 
-static void sector_printf(void * buf)
+static void sector_printf(void* buf)
 {
 	int i, j, k;
 
-	for(i=0; i<(128/8); i++)
+	for (i = 0; i < (128 / 8); i++)
 	{
-		printf("[%3d]", i*32);
+		printf("[%3d]", i * 32);
 
-		for(j=0; j<8; j++)
+		for (j = 0; j < 8; j++)
 		{
-			for(k=0; k<4; k++)
+			for (k = 0; k < 4; k++)
 			{
-				printf("%.2X", ((char *)buf)[i*32+j*4+k]);
+				printf("%.2X", ((char*)buf)[i * 32 + j * 4 + k]);
 			}
 			printf(" ");
 		}
@@ -50,32 +50,32 @@ static void sector_printf(void * buf)
 void main(void)
 {
 	int i, k;
-	int buf[512/4];
+	int buf[512 / 4];
 
 	printf("SD Card Access Test\n");
-	if(SD_Check_Card() != SD_SUCCESS) printf("SD Card를 삽입해 주십시오\n");
+	if (SD_Check_Card() != SD_SUCCESS) printf("SD Card를 삽입해 주십시오\n");
 
-	for(;;)
+	for (;;)
 	{
-		if(SD_Check_Card() == SD_SUCCESS) break;
+		if (SD_Check_Card() == SD_SUCCESS) break;
 	}
 
 	printf("SD_Check_Card() Passed!\n");
 
-	i=SD_Init();
+	i = SD_Init();
 	printf("SD_Init() Passed!\n");
-	if(i != SD_SUCCESS) printf("Init Err : [%d]\n", i);
+	if (i != SD_SUCCESS) printf("Init Err : [%d]\n", i);
 
 	Key_Poll_Init();
 
-	for(k=0;k<2;k++)
+	for (k = 0; k < 2; k++)
 	{
 		printf("아무 KEY를 누르면 시작합니다\n");
 
 		Key_Wait_Key_Pressed();
 		Key_Wait_Key_Released();
 
-		SD_Read_Sector(k, 1, (U8 *)buf);
+		SD_Read_Sector(k, 1, (U8*)buf);
 		printf("\n[%d]번 Sector Read!\n", k);
 		sector_printf(buf);
 	}
@@ -101,13 +101,13 @@ void main(void)
 
 	add = Uart_GetIntNum();
 
-	if((add <= 0x30000000) || (add >= 0x32000000))
+	if ((add <= 0x30000000) || (add >= 0x32000000))
 	{
 		add = 0x31000000;
 	}
 
 	printf("%#x\n", add);
-	Lcd_Draw_BMP_File_24bpp(0,0,(void *)add);
+	Lcd_Draw_BMP_File_24bpp(0, 0, (void*)add);
 }
 
 #endif
@@ -125,35 +125,75 @@ void main(void)
 #pragma pack(push, 1)
 
 /* MBR을 모델링하기 위한 구조체 타입 선언 */
-
-typedef struct 
-{
+typedef struct {
 	unsigned char rsvd[8];
 	unsigned int LBA_Start;
 	unsigned int Size;
-} PARTITION;
+}PARTITION;
 
 typedef struct {
 	unsigned char rsvd[446];
 	PARTITION part[4];
 	unsigned short signature;
-} MBR;
-
+}MBR;
 /* BR을 모델링하기 위한 구조체 타입 선언 */
-
+typedef struct {
+	unsigned char boot[11];
+	unsigned short bytepersector;
+	unsigned char sectorpercluster;
+	unsigned short rsvdsectorcnt;
+	unsigned char nooffats;
+	unsigned short rootenterycnt;
+	unsigned char rsvd1[3];
+	unsigned short fat16size;
+	unsigned char rsvd2[8];
+	unsigned int totalsector;
+}BR;
 
 /* File Entry 분석을 위한 구조체, 공용체 타입 선언 */
-
+typedef struct {
+	unsigned char r : 1;
+	unsigned char h : 1;
+	unsigned char s : 1;
+	unsigned char v : 1;
+	unsigned char d : 1;
+	unsigned char a : 1;
+}fat_file;
+typedef struct {
+	unsigned char ln : 4;
+}long_file;
+typedef union {
+	fat_file n;
+	long_file l;
+	unsigned char c;
+}FATATTR;
 /* 시간 포맷을 위한 비트필드 구조체 선언 */
+typedef struct {
+	unsigned short sec : 5;
+	unsigned short min : 6;
+	unsigned short hour : 5;
+}FATTIME;
 /* 날짜 포맷을 위한 비트필드 구조체 선언 */
-
+typedef struct {
+	unsigned short day : 5;
+	unsigned short mon : 4;
+	unsigned short year : 7;
+}FATDATE;
 /* 하나의 32B Entry 분석을 위한 구조체 */
-
 typedef struct
-{
-	/* Entry의 각 멤버를 설계한다 */
-
-
+{/* Entry의 각 멤버를 설계한다 */
+	unsigned char name[11];
+	FATATTR attr;
+	unsigned char rsvd;
+	unsigned char creationtimetenth;
+	FATTIME creationtime;
+	FATDATE creationdate;
+	FATDATE lastaccessdate;
+	unsigned short firstclusterhigh;
+	FATTIME lastwritetime;
+	FATDATE lastwritedate;
+	unsigned short firstclusterlow;
+	unsigned int filesize;
 }ENTRY;
 
 #pragma pack(pop)
@@ -174,30 +214,30 @@ static struct _parameter
 /* 하부 설계되는 함수 목록 */
 
 static void listing_file(void);
-static ENTRY * search_file(int id);
-static int check_file_type(ENTRY * file);
-static void read_file(ENTRY * file, void * data);
-static void sector_printf(void * buf);
+static ENTRY* search_file(int id);
+static int check_file_type(ENTRY* file);
+static void read_file(ENTRY* file, void* data);
+static void sector_printf(void* buf);
 
 /* 512B 단위의 섹터를 읽어서 저장하기 위한 임시 버퍼 */
 
-static unsigned int buf[512/4];
+static unsigned int buf[512 / 4];
 
 /* 512B 단위의 섹터를 인쇄해주는 디버깅용 함수 */
 
-static void sector_printf(void * buf)
+static void sector_printf(void* buf)
 {
 	int i, j, k;
 
-	for(i=0; i<(128/8); i++)
+	for (i = 0; i < (128 / 8); i++)
 	{
-		printf("[%3d]", i*32);
+		printf("[%3d]", i * 32);
 
-		for(j=0; j<8; j++)
+		for (j = 0; j < 8; j++)
 		{
-			for(k=0; k<4; k++)
+			for (k = 0; k < 4; k++)
 			{
-				printf("%.2X", ((char *)buf)[i*32+j*4+k]);
+				printf("%.2X", ((char*)buf)[i * 32 + j * 4 + k]);
 			}
 			printf(" ");
 		}
@@ -212,38 +252,41 @@ void main(void)
 	Lcd_Graphic_Init();
 	Lcd_Clr_Screen(0xf800);
 
-	if(SD_Check_Card() != SD_SUCCESS) printf("Insert SD Card Please!\n");
+	if (SD_Check_Card() != SD_SUCCESS) printf("Insert SD Card Please!\n");
 
-	for(;;)
+	for (;;)
 	{
-		if(SD_Check_Card() == SD_SUCCESS) break;
+		if (SD_Check_Card() == SD_SUCCESS) break;
 	}
 
-	i=SD_Init();
-	if(i != SD_SUCCESS) printf("Init Err : [%d]\n", i);
+	i = SD_Init();
+	if (i != SD_SUCCESS) printf("Init Err : [%d]\n", i);
 
 	/* 사용자 코드 */
 
 	{
 
 #if 01
-		SD_Read_Sector(0, 1, (U8 *)buf);
+		SD_Read_Sector(0, 1, (U8*)buf);
 
 		/* MBR에서 parameter.lba_start 값을 읽어서 저장한다 */
-
-		((MBR*)buf)->part[0].LBA_Start;
+		parameter.lba_start = ((MBR*)buf)->part[0].LBA_Start;
 
 		printf("LBA_Start = %d\n", parameter.lba_start);
 
 #endif
 
-#if 0
-		SD_Read_Sector(parameter.lba_start, 1, (U8 *)buf);
+#if 01
+		SD_Read_Sector(parameter.lba_start, 1, (U8*)buf);
 
 		/* BR에서 정보들을 읽어서 parameter 구조체 멤버들에 저장한다 */
-
-
-
+		parameter.byte_per_sector = ((BR*)buf)->bytepersector;
+		parameter.sector_per_cluster = ((BR*)buf)->sectorpercluster;
+		parameter.fat0_start = parameter.lba_start + ((BR*)buf)->rsvdsectorcnt;
+		parameter.root_start = parameter.fat0_start +
+			((BR*)buf)->fat16size * ((BR*)buf)->nooffats;
+		parameter.root_sector_count = ((BR*)buf)->rootenterycnt * 32 / parameter.byte_per_sector;
+		parameter.file_start = parameter.root_start + parameter.root_sector_count;
 
 		printf("byte_per_sector = %d\n", parameter.byte_per_sector);
 		printf("sector_per_cluster = %d\n", parameter.sector_per_cluster);
@@ -253,29 +296,29 @@ void main(void)
 
 #endif
 
-		for(;;)
+		for (;;)
 		{
-			ENTRY * file;
+			ENTRY* file;
 			int num;
 			int r;
 
-#if 0
+#if 01
 			/* ROOT 분석 & 파일 인쇄 */
 
 			listing_file();
-			
+
 			printf("File Number(Exit => 999)? ");
 			num = Uart_GetIntNum();
 #endif
 
-#if 0
-			if(num == 999) break;
+#if 01
+			if (num == 999) break;
 
 			/* 원하는 번호의 파일 탐색 */
 
 			file = search_file(num);
 
-			if((file == (ENTRY *)0))
+			if ((file == (ENTRY*)0))
 			{
 				printf("Not Supported File\n");
 				continue;
@@ -283,14 +326,14 @@ void main(void)
 
 			/* 제대로 탐색되었는지 확인하기 위한 이름 인쇄 */
 
-			for(i = 0; i < 8; i++) printf("%c", ((char *)file)[i]);
+			for (i = 0; i < 8; i++) printf("%c", ((char*)file)[i]);
 			printf(".");
-			for(i = 8; i < (8+3); i++) printf("%c", ((char *)file)[i]);
+			for (i = 8; i < (8 + 3); i++) printf("%c", ((char*)file)[i]);
 			printf("\n");
 
 #endif
 
-#if 0
+#if 01
 			/* C, TXT, BMP 파일인지 확인 */
 
 			r = check_file_type(file);
@@ -300,32 +343,32 @@ void main(void)
 #endif
 
 #if 0
-			if(r != 0)
+			if (r != 0)
 			{
-				char * data;
+				char* data;
 				int size = file->size;
 
 				/* 파일 크기를 초과하는 섹터 단위의 메모리로 할당을 받아야 한다 */
-				num = 
-				data = malloc(num);
-				
+				num =
+					data = malloc(num);
+
 				/* 파일 데이터 읽기 */
 
 				read_file(file, data);
 
-				switch(r)
+				switch (r)
 				{
-					case 1 :
-					case 2 :
-						for(i = 0; i < size; i++) printf("%c", ((char *)data)[i]);
-						printf("\n");
-						break;
-					case 3 :
-						Lcd_Clr_Screen(0xf800);
-						Lcd_Draw_BMP_File_24bpp(0,0,(void *)data);
-						break;
-					default :
-						break;
+				case 1:
+				case 2:
+					for (i = 0; i < size; i++) printf("%c", ((char*)data)[i]);
+					printf("\n");
+					break;
+				case 3:
+					Lcd_Clr_Screen(0xf800);
+					Lcd_Draw_BMP_File_24bpp(0, 0, (void*)data);
+					break;
+				default:
+					break;
 				}
 
 				free(data);
@@ -341,59 +384,92 @@ void main(void)
 	}
 }
 
-static void listing_file(void)
-{
+static void entryprint(int num, ENTRY* buf) {
+	int i;
+	printf("[%3d] ", num);
+	for (i = 0; i < 8; i++) printf("%c", buf->name[i]);
+	printf(".");
+	for (i = 8; i < 11; i++) printf("%c", buf->name[i]);
+	printf(" 0x%x ", buf->attr.c);
+	printf("%02d/%02d/%02d ", buf->creationdate.year + 1980,
+		buf->creationdate.mon, buf->creationdate.day);
+	printf("%02d:%02d:%02d ", buf->creationtime.hour,
+		buf->creationtime.min, buf->creationtime.sec * 2);
+	printf("%5d %10d\n", buf->firstclusterlow, buf->filesize);
+}
+static void listing_file(void) {
 	int i, j, num = 1;
-
 	/* 타이틀 인쇄 => 임의 변경 가능 */
-
-	printf("[NUM] [NAME .EXT] [AT] [  DATE  ] [TIME] [CLUST] [ SIZE ]\n");
+	printf("[NUM] [NAME   .EXT] [AT] [  DATE  ] [TIME] [CLUST] [ SIZE ]\n");
 	printf("=============================================================\n");
 
-	for(i = 0; i < parameter.root_sector_count; i++)
+	for (i = 0; i < parameter.root_sector_count; i++)
 	{
-		SD_Read_Sector(parameter.root_start + i, 1, (U8 *)buf);
+		SD_Read_Sector(parameter.root_start + i, 1, (U8*)buf);
 
-		for(j = 0; j < (parameter.byte_per_sector / 32); j++)
+		for (j = 0; j < (parameter.byte_per_sector / 32); j++)
 		{
 			/* Name[0]가 0x0이면 인쇄 종료, 0x05, 0xE5이면 삭제파일 Skip */
+			if (((ENTRY*)buf)[j].name[0] == 0x00) goto EXIT;//인쇄 종료
+			if (((ENTRY*)buf)[j].name[0] == 0x05) continue;//삭제파일 Skip
+			if (((ENTRY*)buf)[j].name[0] == 0xE5) continue;//삭제파일 Skip
 			/* 파일 속성이 0x3F 또는 0x0F long file name 이므로 Skip */
+			if (((ENTRY*)buf)[j].attr.l.ln == 0xf) continue;
 			/* Entry 정보 인쇄 */
+			entryprint(num, &((ENTRY*)buf)[j]);
 			/* 인쇄되는 파일 또는 폴더 마다 맨 앞에 1번부터 1씩 증가하며 번호를 인쇄한다 */
+			num++;
 		}
 	}
-
+EXIT:
 	printf("=============================================================\n");
 }
 
-static ENTRY * search_file(int id)
+static ENTRY* search_file(int id)
 {
 	int i, j, num = 1;
 
-	for(i = 0; i < parameter.root_sector_count; i++)
+	for (i = 0; i < parameter.root_sector_count; i++)
 	{
-		SD_Read_Sector(parameter.root_start + i, 1, (U8 *)buf);
+		SD_Read_Sector(parameter.root_start + i, 1, (U8*)buf);
 
-		for(j = 0; j < (parameter.byte_per_sector / 32); j++)
+		for (j = 0; j < (parameter.byte_per_sector / 32); j++)
 		{
 			/* Name[0]가 0x0이면 NULL 포인터 리턴, 0x05, 0xE5이면 Skip */
+			if (((ENTRY*)buf)[j].name[0] == 0x00) return (ENTRY*)0;//인쇄 종료
+			if (((ENTRY*)buf)[j].name[0] == 0x05) continue;//삭제파일 Skip
+			if (((ENTRY*)buf)[j].name[0] == 0xE5) continue;//삭제파일 Skip
 			/* 파일 속성이 0x3F 또는 0x0F long file name 이므로 Skip */
+			if (((ENTRY*)buf)[j].attr.l.ln == 0xf) continue;
 			/* 정상 Entry(파일, 폴더)일 경우 num이 원하는 ID인지 확인 */
 			/* num이 id보다 작으면 num 증가만 하고 계속 탐색 반복, 같으면 Entry 시작 주소 리턴 */
+			if (num == id) return &((ENTRY*)buf)[j];
+			num++;
 		}
 	}
 
-	return (ENTRY *)0;
+	return (ENTRY*)0;
 }
 
-static int check_file_type(ENTRY * file)
+static int check_file_type(ENTRY* file)
 {
+	static char* cmp[] = { "C  ", "TXT", "BMP" };
+	int i;
 	/* 리턴 값 : C 파일 => 1, TXT 파일 => 2, BMP 파일 => 3, 그외 => 0 리턴 */
-
+	if (file->attr.n.d == 1) return 0;//directory 임
+	if (file->attr.n.a == 0) return 0;//file 아님
+	for (i = 8; i < 11; i++) {
+		if (('a' <= file->name[i]) && (file->name[i] <= 'z')) {
+			file->name[i] = file->name[i] - 'a' + 'A';//소문자를 대문자로 변경
+		}
+	}
+	for (i = 0; i < 3; i++) {
+		if (strncmp((char*)& file->name[8], cmp[i], 3) == 0) return i + 1;
+	}
 	return 0;
 }
 
-static void read_file(ENTRY * file, void * data)
+static void read_file(ENTRY* file, void* data)
 {
 	/* 주어진 Entry의 실제 데이터를 읽어서 data 주소에 저장한다 */
 
